6c10592c872871f09b2a8872fd0a512b24921c35
eeherngSMU <eeherng.tan.2023@scis.smu.edu.sg>
2024-09-10 18:32:54 +0800

    setup db

--- frontend-website/src/app/roadmaps/page.tsx
+++ frontend-website/src/app/roadmaps/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { createClient } from '@/utils/supabase/server';
 
-export default function page() {
+export default async function page() {
+    const supabase = createClient();
+    const { data: notes } = await supabase.from("notes").select();
+
     return (
-        <div>
-            Hello senior roadmaps
-        </div>
+        <>
+            <div>
+                Hello senior roadmaps
+            </div>
+            <pre>{JSON.stringify(notes, null, 2)}</pre>
+        </>
+        
     )
 }
