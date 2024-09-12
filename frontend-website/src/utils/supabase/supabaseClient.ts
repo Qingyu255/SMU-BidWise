856908d68edb3cb8dedbb9b6f365a03139fdaedc
eeherngSMU <eeherng.tan.2023@scis.smu.edu.sg>
2024-09-12 15:53:24 +0800

    resolve merge error

--- frontend-website/src/utils/supabase/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Initialize client-side Supabase
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
