0813e1c99374521c4b0cf1c6823485f1ffabf0bc
Qingyu255 <qingyu2555@gmail.com>
2024-09-15 22:55:54 +0800

    resolve merge conflicts

--- frontend-website/src/app/courses/[course_code]/page.tsx
+++ frontend-website/src/app/courses/[course_code]/page.tsx
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
 "use client";
 import { useState, useEffect } from 'react'; 
 import { createClient } from '@supabase/supabase-js';
 import ProfessorButtons from './ProfessorButtons'; 
 import Timetable from './Timetable'; // Import Timetable component
 
-const supabase = createClient('https://qwlcflxsmsozzgdmcmco.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3bGNmbHhzbXNvenpnZG1jbWNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5MzU5NjMsImV4cCI6MjA0MTUxMTk2M30.ANJBVw0uCw4wqsXXpbJbdXm4S6L8KXPbdF8KpNFBvn0');
-=======
-// Import necessary libraries
-import createClient  from '@/utils/supabase/server';
->>>>>>> origin/main
-
 async function getSectionDetails(course_code: string) {
-<<<<<<< HEAD
-=======
-  // initialise supabase client
-  const supabase = createClient();
+  const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+  const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+  const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
   // Step 1: Find the course_id using course_code from the course_info table
->>>>>>> origin/main
   const { data: courseInfo, error: courseInfoError } = await supabase
     .from('course_info')
     .select('id')
@@ -83,21 +74,18 @@ export default function Page({ params }: { params: { course_code: string } }) {
     <>
       <div>Course Code: {course_code.toUpperCase()}</div>
       <span>This page should contain other info like maybe a timetable depiction of available sections, section details, availability, etc.</span>
-<<<<<<< HEAD
       
       <ProfessorButtons professors={professors} onProfessorClick={updateTimetable} />
       
       {selectedProfessor && (
-  <>
-    <div>Selected Professor: {selectedProfessor}</div>
-    <Timetable
-      professorClasses={sections} // Make sure sections contain the filtered data
-      onClassSelect={(classItem: any) => console.log('Class selected:', classItem)}
-    />
-  </>
-)}
-=======
-
+        <>
+          <div>Selected Professor: {selectedProfessor}</div>
+          <Timetable
+            professorClasses={sections} // Make sure sections contain the filtered data
+            onClassSelect={(classItem: any) => console.log('Class selected:', classItem)}
+          />
+        </>
+      )}
       <div>
         <h2>Available Sections: - Shows both 23-24 T2 and 24-25T1 - TODO: add filter for term</h2>
         <ul>
@@ -108,7 +96,6 @@ export default function Page({ params }: { params: { course_code: string } }) {
           ))}
         </ul>
       </div>
->>>>>>> origin/main
     </>
   );
 }  
