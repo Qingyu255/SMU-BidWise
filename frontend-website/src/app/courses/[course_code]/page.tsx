c3618a2d9af6f8d3accdee93d2f21e4737c6230c
kylene11 <129774318+kylene11@users.noreply.github.com>
2024-09-15 22:36:53 +0800

    fix conflicts maybe

--- frontend-website/src/app/courses/[course_code]/page.tsx
+++ frontend-website/src/app/courses/[course_code]/page.tsx
@@ -1,11 +1,24 @@
+<<<<<<< HEAD
+"use client";
+import { useState, useEffect } from 'react'; 
+import { createClient } from '@supabase/supabase-js';
+import ProfessorButtons from './ProfessorButtons'; 
+import Timetable from './Timetable'; // Import Timetable component
+
+const supabase = createClient('https://qwlcflxsmsozzgdmcmco.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3bGNmbHhzbXNvenpnZG1jbWNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5MzU5NjMsImV4cCI6MjA0MTUxMTk2M30.ANJBVw0uCw4wqsXXpbJbdXm4S6L8KXPbdF8KpNFBvn0');
+=======
 // Import necessary libraries
 import createClient  from '@/utils/supabase/server';
+>>>>>>> origin/main
 
 async function getSectionDetails(course_code: string) {
+<<<<<<< HEAD
+=======
   // initialise supabase client
   const supabase = createClient();
 
   // Step 1: Find the course_id using course_code from the course_info table
+>>>>>>> origin/main
   const { data: courseInfo, error: courseInfoError } = await supabase
     .from('course_info')
     .select('id')
@@ -70,6 +83,20 @@ export default function Page({ params }: { params: { course_code: string } }) {
     <>
       <div>Course Code: {course_code.toUpperCase()}</div>
       <span>This page should contain other info like maybe a timetable depiction of available sections, section details, availability, etc.</span>
+<<<<<<< HEAD
+      
+      <ProfessorButtons professors={professors} onProfessorClick={updateTimetable} />
+      
+      {selectedProfessor && (
+  <>
+    <div>Selected Professor: {selectedProfessor}</div>
+    <Timetable
+      professorClasses={sections} // Make sure sections contain the filtered data
+      onClassSelect={(classItem: any) => console.log('Class selected:', classItem)}
+    />
+  </>
+)}
+=======
 
       <div>
         <h2>Available Sections: - Shows both 23-24 T2 and 24-25T1 - TODO: add filter for term</h2>
@@ -81,6 +108,7 @@ export default function Page({ params }: { params: { course_code: string } }) {
           ))}
         </ul>
       </div>
+>>>>>>> origin/main
     </>
   );
 }  
