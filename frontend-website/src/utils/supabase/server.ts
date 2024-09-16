d4e0a99522dbe952cdff2333e092c89090230546
Qingyu255 <qingyu2555@gmail.com>
2024-09-16 23:56:30 +0800

    cache supabase queries using nextjs fetch caching
    revalidates every 300 seconds

--- frontend-website/src/utils/supabase/server.ts
+++ frontend-website/src/utils/supabase/server.ts
@@ -1,6 +1,15 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
+export const createFetch =
+  (options: Pick<RequestInit, "next" | "cache">) =>
+  (url: RequestInfo | URL, init?: RequestInit) => {
+    return fetch(url, {
+      ...init,
+      ...options,
+    });
+  };
+
 export default function createClient() {
   const cookieStore = cookies()
 
@@ -8,22 +17,55 @@ export default function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      global: {
+        fetch: createFetch({
+          next: {
+            revalidate: 300,
+            tags: ["supabase"],
+          },
+        }),
+      },
       cookies: {
-        getAll() {
-          return cookieStore.getAll()
+        get(name: string) {
+          return cookieStore.get(name)?.value;
         },
-        setAll(cookiesToSet) {
+        set(name: string, value: string, options: CookieOptions) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {
-            // The `setAll` method was called from a Server Component.
+            cookieStore.set({ name, value, ...options });
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: "", ...options });
+          } catch (error) {
+            // The `delete` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
         },
       },
     }
+    // {
+    //   cookies: {
+    //     getAll() {
+    //       return cookieStore.getAll()
+    //     },
+    //     setAll(cookiesToSet) {
+    //       try {
+    //         cookiesToSet.forEach(({ name, value, options }) =>
+    //           cookieStore.set(name, value, options)
+    //         )
+    //       } catch {
+    //         // The `setAll` method was called from a Server Component.
+    //         // This can be ignored if you have middleware refreshing
+    //         // user sessions.
+    //       }
+    //     },
+    //   },
+    // }
   )
 }
