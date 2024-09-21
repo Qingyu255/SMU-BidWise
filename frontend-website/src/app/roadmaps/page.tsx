bbc4e2fcb31070c87da77f8beb54a4ff021eca91
eeherngSMU <eeherng.tan.2023@scis.smu.edu.sg>
2024-09-21 11:01:35 +0800

    center roadmap on reload

--- frontend-website/src/app/roadmaps/page.tsx
+++ frontend-website/src/app/roadmaps/page.tsx
@@ -4,21 +4,23 @@ export default async function Page() {
 
 
   return (
-    <div className='flex flex-col overflow-hidden' style={{ width: '100vw', height: '100vh'}}>
-      <div>
-        Hello, this is senior roadmap.
-        <br/>
-        Only Y1S1 clickable for now!!
-      </div>
-      
-      <div className='container w-full h-full'>
-        <Timeline seniorName='Senior3'/>
+    // <div className='w-full h-full'>
+       <div className='flex flex-col overflow-hidden' style={{ width: '100%', height: '100%'}}>
+          <div>
+            Hello, this is senior roadmap.
+            <br/>
+            Only Y1S1 clickable for now!!
+          </div>
+          
+          <div className='container w-10/12 h-screen self-center flex-grow-0'>
+            <Timeline seniorName='Senior3'/>
+          </div>
+            
+          
 
-      </div>
-        
-      
-
-    </div>
+        </div>
+    // </div>
+    
   );
 }
 
