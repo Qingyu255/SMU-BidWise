4a421031e5b8654af199e366d841857f454d36ee
Qingyu255 <qingyu2555@gmail.com>
2024-10-07 20:07:47 +0800

    edit sign in sign up pages

--- frontend-website/src/app/sign-up/[[...sign-up]]/page.tsx
+++ frontend-website/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { SignUp } from '@clerk/nextjs';
+import { poppins } from '@/utils/fonts';
+import Image from 'next/image';
 
 export default function page() {
     return (
-        <div className='py-10 px-10 md:px-20'>
-            <div className='flex flex-col mb-10'>
-                <span>--SMU BidWise logo here--</span>
-                <h1 className='text-2xl md:text-3xl lg:text-4xl font-extrabold tracking-tight'>
-                    BOSS Bidding has never been easier
-                </h1>
+        <div className='flex flex-col items-center justify-center py-10 md:px-20'>
+            <div className='flex flex-col items-center text-center mb-10'>
+                <div className='flex items-center'>
+                    <Image
+                        className='ml-1 inline'
+                        src="/icon.png"
+                        alt="logo"
+                        width={35}
+                        height={35}
+                    />
+                    <div className={`mx-1 text-lg md:text-xl font-semibold text-foreground ${poppins.className}`}>SMU BIDWISE</div>
+                </div>
+                <div className='mt-4 text-3xl md:text-4xl lg:text-5xl font-extrabold tracking-tight'>
+                    Seamless Course Planning
+                </div>
+                <p className='mt-2 text-sm md:text-base lg:text-lg'>
+                    Take control of your module bidding like never before
+                </p>
             </div>
-            <div className='h-full flex justify-center items-center'>
-                <SignUp/>
-            </div>
+            <SignUp />
         </div>
-    )
+    );
 }
