4a3833740851a936d7eb84850d8f77431f51e6b2
deniselim <deniselim0703@gmail.com>
2024-10-20 22:14:15 +0800

    Merge branch 'main' of https://github.com/Qingyu255/SMU-BidWise

---
+++ frontend-website/src/components/UserAvatar.tsx
@@ -0,0 +1,32 @@
+import { AvatarProps } from '@radix-ui/react-avatar'
+import { Icons } from '@/components/Icons'
+import { Avatar, AvatarFallback } from './ui/avatar'
+import Image from 'next/image'
+
+// Update the props to receive `image` and `name` directly, bypassing Prisma User type
+interface UserAvatarProps extends AvatarProps {
+    image?: string | null // Optional profile image from Clerk
+    name?: string | null  // Optional name from Clerk
+  }
+  
+  export function UserAvatar({ image, name, ...props }: UserAvatarProps) {
+    return (
+      <Avatar {...props}>
+        {image ? (
+          <div className='relative aspect-square h-full w-full'>
+            <Image
+              fill
+              src={image} // Profile image URL
+              alt='Profile picture'
+              referrerPolicy='no-referrer'
+            />
+          </div>
+        ) : (
+          <AvatarFallback>
+            <span className='sr-only'>{name}</span>
+            <Icons.user className='h-4 w-4' /> {/* Fallback icon */}
+          </AvatarFallback>
+        )}
+      </Avatar>
+    )
+  }
